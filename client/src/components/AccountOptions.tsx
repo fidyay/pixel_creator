0f8a996f3c1e25dac62505d95dddb39cf7e2e3b8
Fidyay <fidiy1878@gmail.com>
2022-06-15 01:43:08 +0300

    added account options without functionality

--- client/src/components/AccountOptions.tsx
+++ client/src/components/AccountOptions.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StateContext } from "../index";
+import Button from "./Button";
 
 const AccountOptions = () => {
-    
+    const userName = useContext(StateContext).userName
+    return (
+        <div className="home-header__account-options account-options">
+            <h3 className="account-options__heading">Hi👋, {userName}.</h3>
+            <Button className="account-options__button">Change name</Button>
+            <Button className="account-options__button">Change password</Button>
+            <Button className="account-options__button">Log off</Button>
+            <Button className="account-options__button" deleteButton>Delete account</Button>
+        </div>
+    )
 }
+
+export default AccountOptions
--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -6,6 +6,7 @@ import { observer } from "mobx-react";
 import { Outlet, Link } from "react-router-dom";
 import { StateContext } from "../index";
 import Scrollbar from "./Effects/Scrollbar";
+import AccountOptions from "./AccountOptions";
 
 
 const HomePage = observer(() => {
@@ -17,13 +18,15 @@ const HomePage = observer(() => {
             <div className="home">
                 <header className="home__header home-header">
                     <Logo isInMainPage/>
-                    <p className="home-header__description">Free online editor for pixel art and animated sprites</p>
+                    <p className="home-header__description">Free online editor for pixel art and animated sprites.</p>
                     <Button link linkPath="project-configuration" className="home-header__button">Create project</Button>
-                    {(!localStorage.getItem('token') && !userName) && (
+                    {(!localStorage.getItem('token') && !userName) ? (
                         <>
                             <p className="home-header__authorization">Have an account? <Link className="home-header__authorization-link" to='login'>Login here.</Link></p>
                             <p className="home-header__authorization">Wanna create an account? <Link className="home-header__authorization-link" to='sign-in'>Sign in here.</Link></p>
-                        </>
+                        </> 
+                    ) : (
+                        <AccountOptions/>
                     )}
                 </header>
                 {amountOfProjects ? <UserProjects/> : null}
